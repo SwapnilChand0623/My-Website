2245fd3f8aa6c11f070d4b7179a82e0d1917de37
Swapnil Chand <sxc0623@mavs.uta.edu>
2026-05-08 13:31:44 -0500

    Add Netlify contact form

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function toggleDetail(id) {
   const section = document.getElementById(id);
   section.classList.toggle('show');
 }
-
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Thanks for reaching out. I'll get back to you soon.");
-});
